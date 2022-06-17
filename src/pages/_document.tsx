ea41ed7a1d4737ed2a8693b34ca804dc8fce79c3
Leonardo Vargas <leu1607@hotmail.com>
2022-06-17 15:03:29 -0300

    style: add fonte e variaveis de cor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,10 +15,7 @@ export default class MyDocument extends Document {
           <meta property="og:site_name" content="Falconi" />
           <meta name="author" content="Leonardo Luis de Vargas" />
 
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
         </Head>
 
         <body>
